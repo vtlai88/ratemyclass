05680b1671d5d5f4fe10b79c60d135d27842a5ca
Cesar Ayuso <csayuso@ucdavis.edu>
2020-07-03 19:24:09 -0700

    fully updated App.js index.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,55 +1,37 @@
 import React from "react";
+import AppBar from './AppBar';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  Redirect
 } from "react-router-dom";
+
+// from Home branch
+import Home from './pages/mainPage';
+import NotFoundPage from './pages/404';
+import MajorDepartment from './pages/majorDepartmentSearch';
+import Login from './pages/login';
+import SignIn from './pages/signIn';
+
 
 export default function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/signIn">Sign In</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
+        <AppBar/>
+    
         <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/signIn">
-            <SignIn />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/login" component={Login}/>
+          <Route exact path="/signIn" component={SignIn}/>
+          <Route exact path="/majorDepartment" component={MajorDepartment}/>
+          <Route path="/404" component={NotFoundPage}/>
+          <Redirect to="/404" />
         </Switch>
       </div>
     </Router>
   );
-}
+};
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function SignIn() {
-  return <h2>Sign In</h2>;
-}
-
-function Login() {
-  return <h2>Login</h2>;
-}
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import AppBar from './AppBar';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +11,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+//ReactDOM.render(<Demo />, document.querySelector('#root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

dd8df3fee819c4b02c0f7c53a057ef030edd10e7
Cesar Ayuso <csayuso@ucdavis.edu>
2020-07-03 19:19:55 -0700

    merged Valentinas home branch and got redirecting working

---
+++ my-app/src/pages/majorDepartmentSearch.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function MajorDepartment() {
+    return (
+    <div>
+    <h1>major department page
+    </h1>
+    </div>
+    );
+  };
+  
+
+
+
+export default MajorDepartment;
---
+++ my-app/src/pages/signIn.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function SignIn() {
+    return <h2>Sign In</h2>;
+  }
+
+
+export default SignIn;
